e926c832c148a7bdc70151f9483b311b15ed5763
Jemeeeeer <163028167+Jemeeeeer@users.noreply.github.com>
2024-03-15 09:26:03 +0800

    Update PZEM_Final.ino

--- PZEM_Final.ino
+++ PZEM_Final.ino
@@ -1,25 +1,24 @@
-#include <PZEM004Tv30.h>
-#include <LiquidCrystal_I2C.h>
+#include <WebServer.h> //webserver libraries (automatic)
+#include <WiFi.h> //wifi libraries (automatic)
+#include <WiFiClient.h>
+#include <WiFiServer.h>
 
-//acs712 sensor
-const int sensorIn = 34;      
-int mVperAmp = 100;           
-int Watt = 0;
-double Voltage = 0;
-double VRMS = 0;
-double AmpsRMS = 0;
+#include <PZEM004Tv30.h> //pzem library
+#include <LiquidCrystal_I2C.h> //LCD library
 
-//pzem sensor
+//pzem sensor pins
 PZEM004Tv30 pzem1(Serial, 3, 1);
-PZEM004Tv30 pzem2(Serial, 9, 10);
+PZEM004Tv30 pzem2(Serial2, 10, 9);
 
 //LCD
 LiquidCrystal_I2C lcd (0x27, 16,2);
 
+
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   Serial.println ("ACS712 current sensor"); 
   lcd.init();
+  
   lcd.backlight();
   lcd.print ("ACS712 current");
   lcd.setCursor (0, 1);
@@ -30,168 +29,88 @@ void setup() {
 
 void loop() {
 //1st pzem sensor
-  float voltage1 = pzem1.voltage();
-  if(!isnan(voltage1)){
-    Serial.print("Voltage: "); 
-    Serial.print(voltage1); 
-    Serial.println("V");
-  } else {
-    Serial.println("Error reading voltage");
-  }
-  float current1 = pzem1.current();
-  if(!isnan(current1)){
-    Serial.print("Current: "); 
-    Serial.print(current1); 
-    Serial.println("A");
-  } else {
-    Serial.println("Error reading current");
-  }
+    Serial.print("Custom Address:");
+    Serial.println(pzem1.readAddress(), HEX);
+    Serial2.println(pzem2.readAddress(), HEX);
 
-  float power1 = pzem1.power();
-  if(!isnan(power1)){
-    Serial.print("Power: "); 
-    Serial.print(power1); 
-    Serial.println("W");
-  } else {
-    Serial.println("Error reading power");
-  }
-  
-  float energy1 = pzem1.energy();
-  if(!isnan(energy1)){
-    Serial.print("Energy: "); 
-    Serial.print(energy1,3); 
-    Serial.println("kWh");
-  } else {
-    Serial.println("Error reading energy");
-  }
 
-  float frequency1 = pzem1.frequency();
-   if(!isnan(frequency1)){
-    Serial.print("Frequency: "); 
-    Serial.print(frequency1, 1); 
-    Serial.println("Hz");
-  } else {
-    Serial.println("Error reading frequency");
-  }
-  delay(1);
-  float pf1 = pzem1.pf();
-   if(!isnan(pf1)){
-    Serial.print("PF: "); 
-    Serial.println(pf1);
-  } else {
-    Serial.println("Error reading power factor");
-  }
-  
-  Serial.println();
-  delay(2000);
+    // Read the data from the pzem1
+    float voltage1 = pzem1.voltage();      float voltage2 = pzem2.voltage();
+    float current1 = pzem1.current();      float current2 = pzem2.current();
+    float power1 = pzem1.power();          float power2 = pzem2.power();
+    float energy1 = pzem1.energy();        float energy2 = pzem2.energy();
+    float frequency1 = pzem1.frequency();  float frequency2 = pzem2.frequency();
+    float pf1 = pzem1.pf();                float pf2 = pzem2.pf();
 
-//2nd pzem sensor
-  float voltage2 = pzem2.voltage();
-  if(!isnan(voltage2)){
-    Serial.print("Voltage: "); 
-    Serial.print(voltage2); 
-    Serial.println("V");
-  } else {
-    Serial.println("Error reading voltage");
-  }
-  float current2 = pzem2.current();
-  if(!isnan(current2)){
-    Serial.print("Current: "); 
-    Serial.print(current2); 
-    Serial.println("A");
-  } else {
-    Serial.println("Error reading current");
-  }
+    // Check if the data1 is valid
+    if(isnan(voltage1)){
+        Serial.println("Error reading Inverter voltage");
+    } else if (isnan(current1)) {
+        Serial.println("Error reading Inverter current");
+    } else if (isnan(power1)) {
+        Serial.println("Error reading Inverter power");
+    } else if (isnan(energy1)) {
+        Serial.println("Error reading Inverter energy");
+    } else if (isnan(frequency1)) {
+        Serial.println("Error reading Inverter frequency");
+    } else if (isnan(pf1)) {
+        Serial.println("Error reading Inverter power factor");
+    } else {
 
-  float power2 = pzem2.power();
-  if(!isnan(power2)){
-    Serial.print("Power: "); 
-    Serial.print(power2); 
-    Serial.println("W");
-  } else {
-    Serial.println("Error reading power");
-  }
-  
-  float energy2 = pzem2.energy();
-  if(!isnan(energy2)){
-    Serial.print("Energy: "); 
-    Serial.print(energy2,3); 
-    Serial.println("kWh");
-  } else {
-    Serial.println("Error reading energy");
-  }
+        // Print the values to the Serial console
+        Serial.print("Inverter Voltage: ");      Serial.print(voltage1);      Serial.println("V");
+        Serial.print("Inverter Current: ");      Serial.print(current1);      Serial.println("A");
+        Serial.print("Inverter Power: ");        Serial.print(power1);        Serial.println("W");
+        Serial.print("Inverter Energy: ");       Serial.print(energy1,3);     Serial.println("kWh");
+        Serial.print("Inverter Frequency: ");    Serial.print(frequency1, 1); Serial.println("Hz");
+        Serial.print("Inverter PF: ");           Serial.println(pf1);
 
-  float frequency2 = pzem1.frequency();
-   if(!isnan(frequency2)){
-    Serial.print("Frequency: "); 
-    Serial.print(frequency2, 1); 
-    Serial.println("Hz");
-  } else {
-    Serial.println("Error reading frequency");
-  }
-  
-  float pf2 = pzem1.pf();
-   if(!isnan(pf2)){
-    Serial.print("PF: "); 
-    Serial.println(pf2);
-  } else {
-    Serial.println("Error reading power factor");
-  }
-  
-  Serial.println();
-  delay(2000);
+    }
 
+     // Check if the data2 is valid
+    if(isnan(voltage2)){
+        Serial2.println("Error reading Load voltage");
+    } else if (isnan(current2)) {
+        Serial2.println("Error reading Load current");
+    } else if (isnan(power2)) {
+        Serial2.println("Error reading Load power");
+    } else if (isnan(energy2)) {
+        Serial2.println("Error reading Load energy");
+    } else if (isnan(frequency2)) {
+        Serial2.println("Error reading Load frequency");
+    } else if (isnan(pf2)) {
+        Serial2.println("Error reading Load power factor");
+    } else {
+
+        // Print the values to the Serial console
+        Serial2.print("Load Voltage: ");      Serial2.print(voltage2);      Serial2.println("V");
+        Serial2.print("Load Current: ");      Serial2.print(current2);      Serial2.println("A");
+        Serial2.print("Load Power: ");        Serial2.print(power2);        Serial2.println("W");
+        Serial2.print("Load Energy: ");       Serial2.print(energy2,3);     Serial2.println("kWh");
+        Serial2.print("Load Frequency: ");    Serial2.print(frequency2, 1); Serial2.println("Hz");
+        Serial2.print("Load PF: ");           Serial2.println(pf2);
+
+    }
+    Serial.println();
+    delay(2000);
+    
 //acs712 sensor
-  Serial.println (""); 
-  Voltage = getVPP();
-  VRMS = (Voltage/2.0) *0.707;   //root 2 is 0.707
-  AmpsRMS = ((VRMS * 1000)/mVperAmp)-0.1; //0.3 is the error I got for my sensor
- 
-  Serial.print(AmpsRMS);
-  Serial.print(" Amps RMS  ---  ");
-  Watt = ((AmpsRMS*240/1.2)+1);
-  // note: 1.2 is my own empirically established calibration factor
-  // as the voltage measured at D34 depends on the length of the OUT-to-D34 wire
-  // 240 is the main AC power voltage – this parameter changes locally
-  Serial.print(VRMS);
-  Serial.println(" Voltage");
+  int adc = analogRead(A6); //gpio34
+  float voltage= map(adc, 0, 1023, -5  ,5);
+  float current = ((voltage-2.5)/0.185);
+  Serial.print("Battery Voltage: ");
+  Serial.println(voltage);
+  Serial.print("Battery Current: ");
+  Serial.println(current);
+  delay(1000);
+
   lcd.setCursor (0, 0);
-  lcd.print (AmpsRMS);
-  lcd.print (" Amps ");
+  lcd.print (current);
+  lcd.print (" A ");
   //Here cursor is placed on first position (col: 0) of the second line (row: 1) 
   lcd.setCursor (0, 1);
-  lcd.print (VRMS);
+  lcd.print (voltage);
   lcd.print (" V ");
-  delay (2000);
+  delay (1000);
 }
 
-// ***** function calls ******
-float getVPP()
-{
-  float result;
-  int readValue;                // value read from the sensor
-  int maxValue = 0;             // store max value here
-  int minValue = 4096;          // store min value here ESP32 ADC resolution
-  
-   uint32_t start_time = millis();
-   while((millis()-start_time) < 1000) //sample for 1 Sec
-   {
-       readValue = analogRead(sensorIn);
-       // see if you have a new maxValue
-       if (readValue > maxValue) 
-       {
-           /*record the maximum sensor value*/
-           maxValue = readValue;
-       }
-       if (readValue < minValue) 
-       {
-           /*record the minimum sensor value*/
-           minValue = readValue;
-       }
-   }
-   
-   // Subtract min from max
-   result = ((maxValue - minValue) * 3.3)/4096.0; //ESP32 ADC resolution 4096
-      
-   return result;
- }
